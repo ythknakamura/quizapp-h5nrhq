f3868298b9854d14dd80c669599fc7e0490af511
ythknakamura <ythk.nakamura@gmail.com>
2024-01-28 21:24:07 +0900

    完成

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,17 @@
-<script>
-    import AnswerButton from "./AnswerButton.svelte";
-    let currentScore = 0;
-    let renzokuSeikai = 0;
-    let quizdata = {mondai: "鮪", seikai: "まぐろ", taku: ["ひらめ", "かれい", "まぐろ", "ぶり", "くじら"]};
-
-    function answerButtonClicked(isCorrect){
-    if(isCorrect){
-        renzokuSeikai += 1;
-        currentScore += renzokuSeikai;
-    }
-    else{
-        renzokuSeikai = 0;
-    }
-}
-</script>
-
 <main class="flex flex-col h-dvh">
     <!-- メニューバー -->
     <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
-        <div>スコア:{currentScore}</div>
-        <div>連続正解数:{renzokuSeikai}</div>
+        <div>スコア:0</div>
+        <div>連続正解数:0</div>
     </div>
 
     <!-- 問題 -->
-    <div class="bg-green-200 text-center text-4xl py-4">{quizdata.mondai}</div>
+    <div class="bg-green-200 text-center text-4xl py-4">鮪</div>
     
     <!-- 選択肢 -->
     <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
-        {#each quizdata.taku as t}
-            <AnswerButton on:click={() => answerButtonClicked(quizdata.seikai===t)}>{t}</AnswerButton>
-        {/each}
+        <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">まぐろ</button>
+        <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">ぶり</button>
+        <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">くじら</button>
     </div>
 </main>
