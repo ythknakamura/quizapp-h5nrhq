508228f1b348ce3e38f61a9601155a4e794e928c
ythknakamura <ythk.nakamura@gmail.com>
2024-01-29 17:38:17 +0900

    after chap 6

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,34 @@
+<script>
+    import AnswerButton from "./AnswerButton.svelte";
+    let currentScore = 0;
+    let renzoukuSeikai = 0;
+    let quizdata = {mondai: "鮪", seikai: "まぐろ", taku: ["ひらめ", "かれい", "まぐろ", "ぶり", "くじら"]};
+
+    const answerButtonClicked = (isCorrect) => {
+        if(isCorrect){
+            renzoukuSeikai += 1;
+            currentScore += renzoukuSeikai;
+        }
+        else{
+            renzoukuSeikai = 0;
+        }
+    };
+</script>
+
 <main class="flex flex-col h-dvh">
     <!-- メニューバー -->
     <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
-        <div>スコア:0</div>
-        <div>連続正解数:0</div>
+        <div>スコア:{currentScore}</div>
+        <div>連続正解数:{renzoukuSeikai}</div>
     </div>
 
     <!-- 問題 -->
-    <div class="bg-green-200 text-center text-4xl py-4">鮪</div>
+    <div class="bg-green-200 text-center text-4xl py-4">{quizdata.mondai}</div>
     
     <!-- 選択肢 -->
     <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
-        <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">まぐろ</button>
-        <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">ぶり</button>
-        <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">くじら</button>
+        {#each quizdata.taku as t}
+            <AnswerButton on:click={() => answerButtonClicked(quizdata.seikai===t)}>{t}</AnswerButton>
+        {/each}
     </div>
 </main>
